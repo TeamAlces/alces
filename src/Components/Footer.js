8f0a87d8cca33d83db77da09b4c040d22a78997f
Petter Knutsson <xylizch@hotmail.com>
2018-08-28 22:59:18 +0200

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './../Style/App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+      
+    );
+  }
+}
+
+export default Footer;
