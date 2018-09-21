532d09dd9ee63270c97feee07916c5f4574933c2
bubben <darubesandstorm@gmail.com>
2018-09-21 20:25:23 +0200

    added contact and changed section colors

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import Container from '../../components/containers/Container';
 import About from '../../components/Sections/About/About';
 import Projects from '../../components/Sections/Projects/Projects';
+import Contact from '../../components/Sections/Contact/Contact';
 
 import '../../../Style/index.css';
 
@@ -19,8 +20,7 @@ export default class Home extends Component {
           <Header/>
             <About/>  
             <Projects/>
-            <About/>
-            <Projects/>
+            <Contact/>
           <Footer/>
         
       </Container>
