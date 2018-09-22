bd2b87f763fea4b6e626c64f1377ec04b31f9de8
Petter Knutsson <petterknutsson5@gmail.com>
2018-09-22 12:50:54 +0200

    deleted component with array of carousel images

--- src/views/components/Carousel/ImgUrls.js
+++
@@ -1,9 +0,0 @@
-const imgUrls = [
-	"https://cmeimg-a.akamaihd.net/640/clsd/getty/c64f76dc20c246ca88ee180fe4b4b781", 
-	"https://lh3.googleusercontent.com/oxPeODS2m6rYIVbhcQChRtOWEYeGDwbeeeB1cDU2o_WYAVPU61VIgx-_6BAh5gSL8Sw=h900",
-	"https://i0.wp.com/www.universodegatos.com/wp-content/uploads/2017/04/fivfelv7.jpg?resize=582%2C328",
-	"https://i.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg",
-	"https://ehealthforum.com/health/images/avatars/11699147425707699031013.jpeg"
-];
-
-export default imgUrls;
