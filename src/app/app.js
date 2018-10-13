05e1b8000b2ecf97c1b2a2ec0a5be71d7caa21df
Petter Knutsson <petterknutsson5@gmail.com>
2018-10-13 23:42:21 +0200

    added scss,sass to webpack config, extracted stylesheets to bundle in single file, uglifyjs to minify js

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ import Home from './modules/home/home';
 import NoMatch from './modules/404/NoMatch';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import './app.css';
+import './app.sass';
 
 export default class App extends Component {
   render() {
