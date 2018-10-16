1ed2671fff0f13922fe9053d0a09e27a6d3582a6
bubben <darubesandstorm@gmail.com>
2018-10-16 22:27:53 +0200

    replaced footer css with sass and started working on footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Col, Row, Grid } from 'react-flexbox-grid';
-import './Footer.css';
+import './Footer.sass';
 
 export default class Footer extends Component {
   render() {
@@ -8,9 +8,24 @@ export default class Footer extends Component {
       <footer>
         <Grid fluid>
           <Row between="xs" center="md">
-            <Col md={4}>Team Alces &copy;</Col>
-            <Col md={4}>Col 2</Col>
-            <Col md={4}>Col 3</Col>
+          <Col md={4}><p>Copyright &copy; Team Alces</p></Col>
+            <Col md={4}><p>LOGO</p></Col>
+            <Col md={4}>
+            <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                 <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </Col>
           </Row>
         </Grid>
       </footer>
